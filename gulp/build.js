7c95392f7a153e6210b4f32c2160e2295cd87fdc
jupl <juan.placencia.512@gmail.com>
2014-11-03 23:02:54 -0600

    Formatting

--- gulp/build.js
+++ gulp/build.js
@@ -15,15 +15,16 @@ gulp.task(rump.taskName('build:sass'), function() {
                          rump.configs.main.globs.build.sass);
   var destination = path.join(rump.configs.main.paths.destination.root,
                               rump.configs.main.paths.destination.sass);
+  var sourceMap = rump.configs.main.styles.sourceMap;
 
   return gulp
-  .src([source].concat(rump.configs.main.globs.global))
-  .pipe((rump.configs.watch ? plumber : util.noop)())
-  .pipe((rump.configs.main.styles.sourceMap ? sourcemaps.init : util.noop)())
-  .pipe(sass(rump.configs.sass))
-  .pipe(autoprefixer(rump.configs.autoprefixer))
-  .pipe((rump.configs.main.styles.sourceMap ? sourcemaps.write : util.noop)())
-  .pipe(gulp.dest(destination));
+    .src([source].concat(rump.configs.main.globs.global))
+    .pipe((rump.configs.watch ? plumber : util.noop)())
+    .pipe((sourceMap ? sourcemaps.init : util.noop)())
+    .pipe(sass(rump.configs.sass))
+    .pipe(autoprefixer(rump.configs.autoprefixer))
+    .pipe((sourceMap ? sourcemaps.write : util.noop)())
+    .pipe(gulp.dest(destination));
 });
 
 gulp.tasks[rump.taskName('build')].dep.push(rump.taskName('build:sass'));
